70c350605d31dc6ed81eef882e36604b91d8788b
uga-rosa <uga6603@gmail.com>
2022-06-23 05:33:47 +0900

    Move process from lua to typescript AMAP

--- lua/coc_luasnip.lua
+++ lua/coc_luasnip.lua
@@ -1,54 +1,24 @@
-local util = require("vim.lsp.util")
-
 local M = {}
 
-local snip_cache = {}
-local doc_cache = {}
-
-function M.getCompletionItems()
-    local filetypes = require("luasnip.util.util").get_snippet_filetypes()
-    local items = {}
-
-    for i = 1, #filetypes do
-        local ft = filetypes[i]
-        if not snip_cache[ft] then
-            -- ft not yet in cache.
-            local ft_items = {}
-            local ft_table = require("luasnip").get_snippets(ft, { type = "snippets" })
-            if ft_table then
-                for _, snip in pairs(ft_table) do
-                    if not snip.hidden then
-                        table.insert(ft_items, {
-                            word = snip.trigger,
-                            menu = "[LuaSnip]",
-                            info = M.getDocument(snip, ft),
-                        })
-                    end
-                end
-            end
-            snip_cache[ft] = ft_items
-        end
-        vim.list_extend(items, snip_cache[ft])
+function M.getSnippets(ft)
+    local snippets = require("luasnip").get_snippets(ft, {type = "snippets"})
+    if not snippets then
+        return {}
     end
 
-    return items
-end
-
-function M.getDocument(snip, ft)
-    if doc_cache[ft] and doc_cache[ft][snip.id] then
-        return doc_cache[ft][snip.id]
+    local ret = {}
+    for _, snip in ipairs(snippets) do
+        table.insert(ret, {
+            trigger = snip.trigger,
+            name = snip.name,
+            id = snip.id,
+            dscr = snip.dscr,
+            hidden = snip.hidden,
+            docstring = snip:get_docstring()
+        })
     end
 
-    local header = (snip.name or "") .. " _ `[" .. ft .. "]`\n"
-    local docstring = { "", "```" .. vim.bo.filetype, snip:get_docstring(), "```" }
-    local documentation = { header .. "---", (snip.dscr or ""), docstring }
-    documentation = util.convert_input_to_markdown_lines(documentation)
-    documentation = table.concat(documentation, "\n")
-
-    doc_cache[ft] = doc_cache[ft] or {}
-    doc_cache[ft][snip.id] = documentation
-
-    return documentation
+    return ret
 end
 
 return M
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,61 @@ import {VimCompleteItem, ExtensionContext, sources, workspace} from 'coc.nvim';
 
 const {nvim} = workspace
 
+let snip_cache = []
+let doc_cache = []
+
+interface snippets {
+    trigger: string,
+    name: string,
+    id: number,
+    dscr: string,
+    hidden: boolean,
+    docstring: string,
+}
+
+const getCompletionItems = async (): Promise<VimCompleteItem[]> => {
+    const filetypes: string[] = await nvim.eval(`luaeval('require("luasnip.util.util").get_snippet_filetypes()')`) as string[]
+    const items = (await Promise.all(filetypes.map(async (ft: string): Promise<VimCompleteItem[]> => {
+        if (!(ft in snip_cache)) {
+            const ft_snippets: snippets[] = await nvim.eval(`luaeval('require("coc_luasnip").getSnippets("${ft}")')`) as snippets[]
+            const ft_items = await Promise.all(ft_snippets.filter((snip: snippets): boolean => {
+                return !snip.hidden
+            }).map(async (snip: snippets): Promise<VimCompleteItem> => {
+                return {
+                    word: snip.trigger,
+                    menu: '[LuaSnip]',
+                    info: await getDocumentation(snip, ft)
+                }
+            }))
+            snip_cache[ft] = ft_items
+        }
+        return snip_cache[ft]
+    }))).reduce((acc, cur) => acc.concat(cur), [])
+
+    return items
+}
+
+const getDocumentation = async (snip: snippets, ft: string): Promise<string> => {
+    if ((ft in doc_cache) && (snip.id in doc_cache[ft])) {
+        return doc_cache[ft][snip.id]
+    }
+
+    const header = snip.name || '' + ` _ \`[${ft}]\``
+    const docstring = ['', '```' + await nvim.eval('&ft'), snip.docstring, '```']
+    const documentation = [header, '---', snip.dscr || ''].concat(docstring).reduce((acc, cur) => acc + cur + '\n', '')
+
+    if (!(ft in doc_cache)) doc_cache[ft] = {}
+    doc_cache[ft][snip.id] = documentation
+
+    return documentation
+}
+
 export const activate = (context: ExtensionContext) => {
     context.subscriptions.push(
         sources.createSource({
             name: 'coc-luasnip',
             doComplete: async () => {
-                const items = await nvim.eval(`luaeval('require("coc_luasnip").getCompletionItems()')`) as VimCompleteItem[]
+                const items = await getCompletionItems()
                 return {items};
             },
             onCompleteDone: async () => {
